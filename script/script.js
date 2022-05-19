a726a262c5f7733a5cb8ff335ebfb98058685cdf
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 23:02:35 +0300

    adding assets

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 let colors = ["green", "red", "yellow", "blue"];
 let generatedSequence = [];
 let userSequence = [];
+var level = 0;
+
 
 //generating numbers between 0 and 3
 function randomColor(){
