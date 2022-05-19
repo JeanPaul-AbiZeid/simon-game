7b722362dc62ab02f335d637b94f6b2c74e95646
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 22:55:58 +0300

    creating a function

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let colors = ["green", "red", "yellow", "blue"];
+let generatedSequence = [];
+let userSequence = [];
+
+//generating numbers between 0 and 3
+function randomColor(){
+    var index = Math.floor(Math.random() * colors.length);
+    return index;
+}
+
+
+
