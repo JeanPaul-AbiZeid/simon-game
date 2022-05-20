516ceff84bec52d4e92a7f5d5a954a1db043dcd8
jean paul <jeanpaulabizeid@gmail.com>
2022-05-20 09:57:39 +0300

    changing button color on click

--- script/script.js
+++ script/script.js
@@ -1,40 +1,7 @@
 let colors = ["green", "red", "yellow", "blue"];
 let generatedSequence = [];
 let userSequence = [];
-var level = 0;
-
-
-//generating numbers between 0 and 3
-function randomColor(){
-    var index = Math.floor(Math.random() * colors.length);
-    return index;
-}
-
-//sounds
-function playSoundGreen(){
-    var audio = new Audio("./assets/sounds/green.mp3");
-    audio.play();
-}
-
-function playSoundRed(){
-    var audio = new Audio("./assets/sounds/red.mp3");
-    audio.play();
-}
-
-function playSoundYellow(){
-    var audio = new Audio("./assets/sounds/yellow.mp3");
-    audio.play();
-}
-
-function playSoundBlue(){
-    var audio = new Audio("./assets/sounds/blue.mp3");
-    audio.play();
-}
-
-function playSoundWrong(){
-    var audio = new Audio("./assets/sounds/wrong.mp3");
-    audio.play();
-}
+var level = 1;
 
 //linking buttons
 var greenBtn = document.getElementsByClassName("green");
@@ -44,8 +11,48 @@ var blueBtn = document.getElementsByClassName("blue");
 var wrongBtn = document.getElementsByClassName("red");
 var mainBtn = document.getElementsByClassName("main-container");
 
+//generating numbers between 0 and 3
+function randomColor(){
+    var index = Math.floor(Math.random() * colors.length);
+    return index;
+}
+
+//sounds
+function greenClick(){
+    var audio = new Audio("./assets/sounds/green.mp3");
+    audio.play();
+    greenBtn[0].style.backgroundColor = "lightgray";
+}
+
+function redClick(){
+    var audio = new Audio("./assets/sounds/red.mp3");
+    audio.play();
+    redBtn[0].style.backgroundColor = "lightgray";
+}
+
+function yellowClick(){
+    var audio = new Audio("./assets/sounds/yellow.mp3");
+    audio.play();
+    yellowBtn[0].style.backgroundColor = "lightgray";
+}
+
+function blueClick(){
+    var audio = new Audio("./assets/sounds/blue.mp3");
+    audio.play();
+    yellowBtn[0].style.backgroundColor = "lightgray";
+}
+
+function wrongClick(){
+    var audio = new Audio("./assets/sounds/wrong.mp3");
+    audio.play();
+}
+
+
 // inital click to start the game
 mainBtn[0].addEventListener("click", function(){
-    
+    greenBtn[0].addEventListener("click", greenClick);
+    redBtn[0].addEventListener("click", redClick);
+    yellowBtn[0].addEventListener("click", yellowClick);
+    blueBtn[0].addEventListener("click", blueClick);
 })
 
